31eeb2878b009ae2aa5e2d009e2c4a71f23abe99
codeliu6572 <codeliu6572@163.com>
2016-06-12 11:01:03 +0800

    Initial Commit

---
+++ 无限轮播/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  无限轮播
+//
+//  Created by 刘浩浩 on 16/6/12.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 无限轮播/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  无限轮播
+//
+//  Created by 刘浩浩 on 16/6/12.
+//  Copyright © 2016年 CodingFire. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
